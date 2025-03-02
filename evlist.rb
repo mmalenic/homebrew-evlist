6b4962908dfe43604f1a3f3ab5416adeece49f13
Marko Malenic <mmalenic1@gmail.com>
2025-03-02 15:06:24 +1100

    style: formatting changes

--- evlist.rb
+++ evlist.rb
@@ -5,13 +5,13 @@ class Evlist < Formula
   desc "List input event devices on Linux"
   homepage "https://github.com/mmalenic/evlist"
   url "https://github.com/mmalenic/evlist/archive/refs/tags/v1.0.0.tar.gz"
-  head "https://github.com/mmalenic/evlist.git", branch: "main"
   sha256 "ebdf35057571ad82c3263ff758217297d6f198fa05bc9b961b3d5a8a7b0731f5"
+  head "https://github.com/mmalenic/evlist.git", branch: "main"
   license "MIT"
 
   bottle do
     root_url "https://github.com/mmalenic/evlist"
-    sha256 cellar: :any_skip_relocation,
+    sha256 cellar:       :any_skip_relocation,
            x86_64_linux: "9ddf9d43ac47ed63cb8f066eedb8a1231e8d0e4ed85c238ec3cb3fc88a285b8f"
   end
 
@@ -50,7 +50,7 @@ class Evlist < Formula
   test do
     assert shell_output("#{bin}/evlist") != ""
     assert_equal "\"NAME\",\"DEVICE_PATH\",\"BY_ID\",\"BY_PATH\",\"CAPABILITIES\"\n",
-                 shell_output("#{bin}/evlist --format csv --filter device_path ''")
+                 shell_output("bin/evlist --format csv --filter device_path ''")
 
     (testpath / "test.cpp").write <<~CPP
       #include <evlist/evlist.h>
