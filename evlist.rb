df230e313f03994815fbdc5306ea5be9850e8d67
Marko Malenic <mmalenic1@gmail.com>
2025-03-02 15:12:04 +1100

    style: more formatting changes

--- evlist.rb
+++ evlist.rb
@@ -6,8 +6,8 @@ class Evlist < Formula
   homepage "https://github.com/mmalenic/evlist"
   url "https://github.com/mmalenic/evlist/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "ebdf35057571ad82c3263ff758217297d6f198fa05bc9b961b3d5a8a7b0731f5"
-  head "https://github.com/mmalenic/evlist.git", branch: "main"
   license "MIT"
+  head "https://github.com/mmalenic/evlist.git", branch: "main"
 
   bottle do
     root_url "https://github.com/mmalenic/evlist"
@@ -48,16 +48,16 @@ class Evlist < Formula
   end
 
   test do
-    assert shell_output("#{bin}/evlist") != ""
+    assert shell_output(bin / "evlist") != ""
     assert_equal "\"NAME\",\"DEVICE_PATH\",\"BY_ID\",\"BY_PATH\",\"CAPABILITIES\"\n",
-                 shell_output("bin/evlist --format csv --filter device_path ''")
+                 shell_output(bin / "evlist --format csv --filter device_path ''")
 
     (testpath / "test.cpp").write <<~CPP
       #include <evlist/evlist.h>
 
       int main() {
         evlist::InputDeviceLister list{};
-        auto devices = list.list_input_devices();#{" "}
+        auto devices = list.list_input_devices();
 
         return !devices.has_value();
       }
